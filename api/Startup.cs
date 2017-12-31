04008dfac9acb2af4fe32611b2ac06eaa6f797e3
Michał Sitko <mic.sitko@gmail.com>
2017-12-31 19:20:59 +0100

    fix

--- api/Startup.cs
+++ api/Startup.cs
@@ -41,10 +41,10 @@ namespace api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
-            }
+            //}
 
             using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
             {
